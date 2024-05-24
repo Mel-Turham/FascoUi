e43a81023e55ad356b816bdd3e77d98471fc6ba6
Mel/ <tchoikuemel06@gmail.com>
2024-05-24 02:05:13 +0100

    Add strictmode

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { ViewProvider } from './Context/ViewContext';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-	<ViewProvider>
-		<App/>
-	</ViewProvider>,
+	<React.StrictMode>
+		<ViewProvider>
+			<App />
+		</ViewProvider>
+	</React.StrictMode>,
 );
